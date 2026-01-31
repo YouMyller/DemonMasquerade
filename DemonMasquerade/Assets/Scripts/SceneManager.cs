9b6e1397f21ca66a96ba66f776848fdd094d644a
Juuso Myllylä <juumyllyla@gmail.com>
2026-01-31 19:20:36 +0200

    Added title screen

---
+++ DemonMasquerade/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StateManager : MonoBehaviour
+{
+    private GameObject titleStuff;
+    private GameObject creditsStuff;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        titleStuff = GameObject.Find("TitleStuff");
+        creditsStuff = GameObject.Find("CreditsStuff");
+
+        creditsStuff.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Arena_new");
+    }
+
+    public void ViewCredits()
+    {
+        titleStuff.SetActive(false);
+        creditsStuff.SetActive(true);
+    }
+
+    public void BackToTitle()
+    {
+        creditsStuff.SetActive(false);
+        titleStuff.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
