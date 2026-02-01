3ed75d04f918b3fa8617501904dd3f76207c9a1d
DonAntti <48753440+DonAntti@users.noreply.github.com>
2026-02-01 10:31:42 +0200

    Enemy Animation + ammo effects

--- DemonMasquerade/Assets/Scripts/EnemySpawner.cs
+++ DemonMasquerade/Assets/Scripts/EnemySpawner.cs
@@ -5,10 +5,14 @@ public class EnemySpawner : MonoBehaviour
     //public GameObject enemy;
     private float timerToSpawn;
     private float spawnNow;
+
+    private Vector3 boost;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timerToSpawn = 3;
+        boost = new Vector3(0, 1, 0);
     }
 
     // Update is called once per frame
@@ -23,8 +27,8 @@ public class EnemySpawner : MonoBehaviour
             //enemyInstance = Instantiate(enemy, this.transform.position, this.transform.rotation);
 
             GameObject Enemy = ObjectPool.SharedInstance.GetEnemy();
-            Enemy.transform.position = this.transform.position;
-            Enemy.transform.rotation = this.transform.rotation;
+            Enemy.transform.position = this.transform.position + boost;
+            //Enemy.transform.rotation = this.transform.rotation;
             Enemy.GetComponentInChildren<EnemyHealth>().SetHealth();
 
             //Ammo.transform.localScale = transform.localScale / 2;
